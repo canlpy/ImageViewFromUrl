72c0216e92c39cf75f7262022b9c99c10f9bc487
Can Alpay <can.alpay@gmail.com>
2023-07-20 18:43:13 +0300

    Initial Commit

---
+++ ImageViewFromUrl/ImageViewFromUrlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewFromUrlApp.swift
+//  ImageViewFromUrl
+//
+//  Created by Can Alpay on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewFromUrlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
